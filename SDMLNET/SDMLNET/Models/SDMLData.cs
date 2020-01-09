d57455ecf055f448695103904a578767b5562d2a
unknown <lin87542296@gmail.com>
2020-01-09 10:53:06 +0800

    Emternet-MLdotNET

---
+++ SDMLNET/SDMLNET/Models/SDMLData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.ML.Data;
+
+namespace SDMLNET.Models
+{
+    class SDMLData
+    {
+        [LoadColumn(11)]
+        public float Sleep { get; set; }
+
+        [LoadColumn(12)]
+        public float Sleeptime { get; set; }
+
+        [LoadColumn(13)]
+        public float AvgHeartRate { get; set; }
+
+        [LoadColumn(14)]
+        public float SocialApp { get; set; }
+
+        [LoadColumn(15)]
+        public float GameApp { get; set; }
+
+        [LoadColumn(16)]
+        public float FunApp { get; set; }
+
+        [LoadColumn(17)]
+        public float OtherApp { get; set; }
+
+        [LoadColumn(18)]
+        public float Temperture { get; set; }
+
+        [LoadColumn(19)]
+        public float Humidity { get; set; }
+
+        [LoadColumn(20)]
+        public float Sunshine { get; set; }
+
+        [LoadColumn(21)]
+        public float Airquality { get; set; }
+
+        [LoadColumn(10)]
+        public string Score { get; set; }
+    }
+    class EmoPredict
+    {
+        [ColumnName("PredictedLabel")]
+        public string Score { get; set; }
+    } 
+}
---
+++ SDMLNET/SDMLNET/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Transforms;
+using SDMLNET.Models;
+using static Microsoft.ML.DataOperationsCatalog;
+
+
+namespace SDMLNET
+{
+    class Program
+    {
+
+        private static string _appPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+        private static string _DataPath => Path.Combine(_appPath, "..", "..", "..", "Data", "XwithLosses.csv");
+        //D:\VS\SDMLNET\SDMLNET\Data
+
+        private static string _modelPath => Path.Combine(_appPath, "..", "..", "..", "Models", "EmoModel2.zip");
+
+
+        public static IDataView data;
+        public static IDataView trainData;
+        public static IDataView testData;
+
+
+        private static MLContext mlContext;
+        //private static PredictionEngine<SDMLData, EmoPredict> _predEngine;
+      
+       
+
+
+
+        static void Main(string[] args)
+        {
+            mlContext = new MLContext();
+
+            data = mlContext.Data.LoadFromTextFile<SDMLData>(_DataPath, hasHeader: true,separatorChar: ',');
+
+            TrainTestData dataSplit = mlContext.Data.TrainTestSplit(data, testFraction: 0.2);
+            trainData = dataSplit.TrainSet;
+            testData = dataSplit.TestSet;
+
+            var model = Train();
+
+            Evaluate(mlContext, model,trainData.Schema);
+        }
+        public static ITransformer Train()
+        {
+
+
+            var pipeline = mlContext.Transforms.Conversion.MapValueToKey(inputColumnName: "Score", outputColumnName: "Label")
+                .Append(mlContext.Transforms.Concatenate("Features", "Sleep", "Sleeptime", "AvgHeartRate", "SocialApp", "GameApp", "FunApp", "OtherApp", "Temperture", "Humidity", "Sunshine", "Airquality"))
+                .Append(mlContext.Transforms.NormalizeMinMax("Features"))
+                .Append(mlContext.MulticlassClassification.Trainers.LbfgsMaximumEntropy("Label", "Features"))
+                .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+
+            var Model = pipeline.Fit(trainData);
+
+            return Model;
+        }
+
+        
+        private static void Evaluate(MLContext mlContext, ITransformer model, DataViewSchema trainDataview)
+         {
+
+            var predictions = model.Transform(testData);
+            var Metrics = mlContext.MulticlassClassification.Evaluate(predictions);
+            //SaveModelAsFile(mlContext, model , trainDataview);
+            
+
+            Console.WriteLine($"*************************************************************************************************************");
+            Console.WriteLine($"*       Metrics for Multi-class Classification model - Test Data     ");
+            Console.WriteLine($"*------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine($"*       MicroAccuracy:    {Metrics.MicroAccuracy:0.###}");
+            Console.WriteLine($"*       MacroAccuracy:    {Metrics.MacroAccuracy:0.###}");
+            Console.WriteLine($"*       LogLoss:          {Metrics.LogLoss:#.###}");
+            Console.WriteLine($"*       LogLossReduction: {Metrics.LogLossReduction:#.###}");
+            Console.WriteLine($"*************************************************************************************************************");
+
+
+
+
+        }
+        private static void SaveModelAsFile(MLContext mlContext,  ITransformer model ,DataViewSchema trainingDataViewSchema)
+        {
+            mlContext.Model.Save(model, trainingDataViewSchema, _modelPath);
+        }
+
+    }
+}
